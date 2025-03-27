750c10f8d165431e590de5fdacaa853cfb1e9d42
minhhalvxxxx <thanvulong@gmail.com>
2025-03-27 15:42:39 +0700

    first commit

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,6 +2,6 @@ import mongoose from "mongoose";
 
 export const  connectDB = async () =>{
 
-    await mongoose.connect('mongodb+srv://thanvulong:ha123456@cluster0.bh8xr.mongodb.net/food-del').then(()=>console.log("DB Connected"));
+    await mongoose.connect(
    
 }
